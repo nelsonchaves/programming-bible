e860a72a9f68869e7017ed9d29033a0fe35350df
Nelson Chaves <nelsonchavespro@gmail.com>
2022-09-07 19:00:30 -0700

    updates

---
+++ Interviews/fizzbuzz.rb
@@ -0,0 +1,53 @@
+# Write a program that prints the numbers from 1 to 100.
+# But for multiples of three print "Fizz" instead of the number
+# and for the multiples of five print "Buzz".
+# For numbers which are multiples of both three and five print "FizzBuzz".
+
+# 1
+def fizzbuzz(number)
+  i = 1
+  until i > number
+    if i % 3 == 0 && i % 5 == 0
+      puts "FizzBuzz for #{i}"
+    elsif i % 3 == 0
+      puts "Fizz for #{i}"
+    elsif i % 5 == 0
+      puts "Buzz for #{i}"
+    else
+      puts i
+    end
+    i += 1
+  end
+end
+
+fizzbuzz(100)
+
+# 2
+(1..100).each do |i|
+  if i % 3 == 0 && i % 5 == 0
+    puts "FizzBuzz for #{i}"
+  elsif i % 3 == 0
+		puts "Fizz for #{i}"
+	elsif i % 5 == 0
+		puts "Buzz for #{i}"
+  else
+  	puts i
+	end
+end
+
+# 3
+(1..100).each { |i| puts i % 3 == 0 ? (i % 5 == 0 ? "FizzBuzz" : "Fizz") : (i % 5 == 0 ? "Buzz" : i) }
+
+# 4
+puts((1..100).map do |i|
+  if i % 3 == 0 && i % 5 == 0
+    "FizzBuzz for #{i}"
+  elsif i % 3 == 0
+		"Fizz for #{i}"
+	elsif i % 5 == 0
+		"Buzz for #{i}"
+	else
+    i
+  end
+end.join("\n")
+)
