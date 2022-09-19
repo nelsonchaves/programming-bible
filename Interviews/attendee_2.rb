e686cca8de37f8d9b2e4f9dcb3ccb370c05d17a1
nelsonchaves <nelsonchavespro@gmail.com>
2022-09-18 23:24:09 -0700

    added attendee 2

---
+++ Interviews/attendee_2.rb
@@ -0,0 +1,29 @@
+class Attendee
+  def initialize(height)
+    @height = height
+  end
+
+  def issue_pass!(pass_id)
+    @pass_id = pass_id
+  end
+
+  def revoke_pass!
+    @pass_id = nil
+  end
+
+  def has_pass?
+    !@pass_id.nil?
+  end
+
+  def fits_ride?(ride_minimum_height)
+    @height >= ride_minimum_height
+  end
+
+  def allowed_to_ride?(ride_minimum_height)
+    fits_ride?(ride_minimum_height) && has_pass?
+  end
+end
+
+attendee = Attendee.new(100)
+p attendee.issue_pass!(42)
+p attendee.allowed_to_ride?(120)
