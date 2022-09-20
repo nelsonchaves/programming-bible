435e89849c9f0e02be544fa34520433ee731d8e3
nelsonchaves <nelsonchavespro@gmail.com>
2022-09-20 10:41:00 -0700

    update

--- Interviews/fizzbuzz.rb
+++ Interviews/fizzbuzz.rb
@@ -4,9 +4,26 @@
 # For numbers which are multiples of both three and five print "FizzBuzz".
 
 # 1
+# def fizzbuzz(number)
+#   i = 1
+#   until i > number
+#     if i % 3 == 0 && i % 5 == 0
+#       puts "FizzBuzz for #{i}"
+#     elsif i % 3 == 0
+#       puts "Fizz for #{i}"
+#     elsif i % 5 == 0
+#       puts "Buzz for #{i}"
+#     else
+#       puts i
+#     end
+#     i += 1
+#   end
+# end
+#
+# fizzbuzz(100)
+
 def fizzbuzz(number)
-  i = 1
-  until i > number
+  (1..number).each do |i|
     if i % 3 == 0 && i % 5 == 0
       puts "FizzBuzz for #{i}"
     elsif i % 3 == 0
@@ -16,38 +33,38 @@ def fizzbuzz(number)
     else
       puts i
     end
-    i += 1
   end
 end
 
 fizzbuzz(100)
 
+
 # 2
-(1..100).each do |i|
-  if i % 3 == 0 && i % 5 == 0
-    puts "FizzBuzz for #{i}"
-  elsif i % 3 == 0
-		puts "Fizz for #{i}"
-	elsif i % 5 == 0
-		puts "Buzz for #{i}"
-  else
-  	puts i
-	end
-end
-
-# 3
-(1..100).each { |i| puts i % 3 == 0 ? (i % 5 == 0 ? "FizzBuzz" : "Fizz") : (i % 5 == 0 ? "Buzz" : i) }
-
-# 4
-puts((1..100).map do |i|
-  if i % 3 == 0 && i % 5 == 0
-    "FizzBuzz for #{i}"
-  elsif i % 3 == 0
-		"Fizz for #{i}"
-	elsif i % 5 == 0
-		"Buzz for #{i}"
-	else
-    i
-  end
-end.join("\n")
-)
+# (1..100).each do |i|
+#   if i % 3 == 0 && i % 5 == 0
+#     puts "FizzBuzz for #{i}"
+#   elsif i % 3 == 0
+# 		puts "Fizz for #{i}"
+# 	elsif i % 5 == 0
+# 		puts "Buzz for #{i}"
+#   else
+#   	puts i
+# 	end
+# end
+#
+# # 3
+# (1..100).each { |i| puts i % 3 == 0 ? (i % 5 == 0 ? "FizzBuzz" : "Fizz") : (i % 5 == 0 ? "Buzz" : i) }
+#
+# # 4
+# puts((1..100).map do |i|
+#   if i % 3 == 0 && i % 5 == 0
+#     "FizzBuzz for #{i}"
+#   elsif i % 3 == 0
+# 		"Fizz for #{i}"
+# 	elsif i % 5 == 0
+# 		"Buzz for #{i}"
+# 	else
+#     i
+#   end
+# end.join("\n")
+# )
