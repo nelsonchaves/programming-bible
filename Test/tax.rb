5b3b19ba1e2e63953c04e0be290c7a6efd1ea065
Nelson Chaves <nelsonchavespro@gmail.com>
2022-09-20 20:33:03 -0700

    update

--- Test/tax.rb
+++ Test/tax.rb
@@ -1,33 +1,19 @@
-def interest(amount)
+class TaxIncome
+  def self.tax_bracket(amount)
     case amount
     when 1..10000
-        20
+      0.2
     when 10000..20000
-        30
+      0.3
     when 20000..50000
-        40
+      0.4
     when 50000..100000
-        50
+      0.5
     end
-end
+  end
 
-def tax(balance)
-  (interest(balance) * balance)/100
-end
-
-p tax(20000)
-def fizzbuzz(number)
-  i = 1
-  until i > number
-    if i % 3 == 0 && i % 5 == 0
-      puts "FizzBuzz for #{i}"
-    elsif i % 3 == 0
-      puts "Fizz for #{i}"
-    elsif i % 5 == 0
-      puts "Buzz for #{i}"
-    else
-      puts i
-    end
-    i += 1
+  def tax(balance)
+    (interest(balance) * balance)/100
   end
 end
+p TaxIncome.tax(100)
